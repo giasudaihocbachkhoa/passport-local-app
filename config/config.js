c388ddf049af902eae29ae4923e9979c2f1e1725
giasudaihocbachkhoa <45159025+giasudaihocbachkhoa@users.noreply.github.com>
2018-12-02 14:41:22 +0700

    Initial commit

---
+++ config/config.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'yourpassword'
+    },
+	'database': 'Nguyen_schema',
+    'users_table': 'users'
+};
---
+++ readme.md
@@ -0,0 +1,1 @@
+# passport-local-app
